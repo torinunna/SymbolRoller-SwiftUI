318a6786b3c03992a6ab5bd747415e796ae83df3
torinunna <torinunna9588@gmail.com>
2023-06-16 14:36:28 +0900

    remove ContntView, create SymbolRollerView

---
+++ SymbolRoller-SwiftUI/SymbolRollerView.swift
@@ -0,0 +1,23 @@
+//
+//  SymbolRollerView.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by YUJIN KWON on 2023/06/16.
+//
+
+import SwiftUI
+
+struct SymbolRollerView: View {
+    
+    let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SymbolRollerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolRollerView()
+    }
+}
