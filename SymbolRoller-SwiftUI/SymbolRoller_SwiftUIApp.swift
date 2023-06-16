d917158fc10d71d3b661b1eea4e212187970c06f
torinunna <torinunna9588@gmail.com>
2023-06-16 13:13:35 +0900

    Initial Commit

---
+++ SymbolRoller-SwiftUI/SymbolRoller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRoller_SwiftUIApp.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by YUJIN KWON on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRoller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
