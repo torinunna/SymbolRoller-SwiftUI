8ea0370df91cd919865d3791b1f675fc1071f32d
torinunna <torinunna9588@gmail.com>
2023-06-17 00:42:52 +0900

    add modifier to SymbolRollerView

--- SymbolRoller-SwiftUI/SymbolRollerView.swift
+++ SymbolRoller-SwiftUI/SymbolRollerView.swift
@@ -16,12 +16,20 @@ struct SymbolRollerView: View {
     var body: some View {
         VStack {
             Image(systemName: imageName)
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
             Text(imageName)
+                .font(.system(size: 40, weight: .bold))
             Button {
                 print("Button Tapped")
             }label: {
                  Text("Reload")
             }
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(.pink)
+            .cornerRadius(40)
         }
     }
 }
